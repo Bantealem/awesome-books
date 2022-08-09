4084af2157a2b3c3a832c66871e4651e9abce8fd
sonickmumba <kingellie.mumba@gmail.com>
2022-08-09 10:38:36 +0200

    Add remove function to menu.js file and fix linter errors

--- js/menu.js
+++ js/menu.js
@@ -1,67 +1,75 @@
-const form = document.querySelector(".form");
-const library = document.querySelector(".library");
-const inputAuthor = document.querySelector(".input-author");
-const inputBook = document.querySelector(".input-book");
-const errormessage = document.querySelector(".errormessage");
-let storedBooks = JSON.parse(localStorage.getItem("books"));
+const form = document.querySelector('.form');
+const library = document.querySelector('.library');
+const inputAuthor = document.querySelector('.input-author');
+const inputBook = document.querySelector('.input-book');
+const errorMessage = document.querySelector('.errormessage');
+const storedBooks = JSON.parse(localStorage.getItem('books'));
 let bookShelf = [];
 let filter = [];
-let counter ="0"
+
 function libraryBooks(object) {
-  return  `<div class="${object.author}">
+  return `<div class="${object.author}">
     <h1>${object.book}</h1>
     <p>${object.author}</p> 
     <hr> 
     <button class="remove">
     remove
     </button>
-    </div>`
+    </div>`;
+}
+
+function remove() {
+  if (bookShelf.length > 0) {
+    const removebtn = document.querySelectorAll('.remove');
+    removebtn.forEach((element) => element.addEventListener('click', () => {
+      const parentNodeClass = element.parentNode.className;
+      element.parentNode.remove();
+      bookShelf = bookShelf.filter((x) => x.author !== parentNodeClass);
+      localStorage.setItem('books', JSON.stringify(bookShelf));
+    }));
   }
-  
+}
+
 function add() {
-  if(inputAuthor.value != "" && inputBook.value != ""){
-    currentBook = []
-    currentBook.push (
+  if (inputAuthor.value !== '' && inputBook.value !== '') {
+    const currentBook = [];
+    currentBook.push(
       {
         author: inputAuthor.value,
-        book: inputBook.value
-      }
-    )
-    filter = bookShelf.filter(x => x.book === currentBook[0].book)
-    if(filter.length > 0){
-      errormessage.style.display ="unset"
-      inputAuthor.value = ""
-      inputBook.value = ""
-      setTimeout(()=>errormessage.style.display = "none",3000)
-      return
+        book: inputBook.value,
+      },
+    );
+    filter = bookShelf.filter((x) => x.book === currentBook[0].book);
+    if (filter.length > 0) {
+      errorMessage.style.display = 'unset';
+      inputAuthor.value = '';
+      inputBook.value = '';
     }
-    bookShelf.push (
+    bookShelf.push(
       {
         author: inputAuthor.value,
-        book: inputBook.value
-      }
-    )   
-     if(bookShelf.length>0) {
-      currentBook.forEach(book => library.insertAdjacentHTML('beforeend', libraryBooks(book)))
+        book: inputBook.value,
+      },
+    );
+    if (bookShelf.length > 0) {
+      currentBook.forEach((book) => library.insertAdjacentHTML('beforeend', libraryBooks(book)));
     }
-  } 
-  inputAuthor.value = ""
-  inputBook.value = ""
-  localStorage.setItem('books', JSON.stringify(bookShelf))
+  }
+  inputAuthor.value = '';
+  inputBook.value = '';
+  localStorage.setItem('books', JSON.stringify(bookShelf));
 }
 
-form.addEventListener("submit", function(event) {
+form.addEventListener('submit', (event) => {
   event.preventDefault();
-  add()
-  remove()
-}
-)
+  add();
+  remove();
+});
 
-if(storedBooks !== null){
-   console.log("entré")
-   bookShelf = storedBooks
-   bookShelf.forEach(book => {
-   library.insertAdjacentHTML('beforeend', libraryBooks(book))
-   remove()
-  })
+if (storedBooks !== null) {
+  bookShelf = storedBooks;
+  bookShelf.forEach((book) => {
+    library.insertAdjacentHTML('beforeend', libraryBooks(book));
+    remove();
+  });
 }
