b29d68510ecda45d55bc3d7530ad36ab0dc22658
Bantealem <bantem2021@gmail.com>
2022-08-11 09:30:10 +0300

    full website

--- js/menu.js
+++ js/menu.js
@@ -1,78 +1,146 @@
-const form = document.querySelector('.form');
-const library = document.querySelector('.library');
-const inputAuthor = document.querySelector('.input-author');
-const inputBook = document.querySelector('.input-book');
-const errorMessage = document.querySelector('.errormessage');
-const storedBooks = JSON.parse(localStorage.getItem('books'));
-let bookShelf = [];
+/*classes for books*/
+
 class Book {
-  constructor(author, book) {
+  constructor(title, author) {
+    this.title = title;
     this.author = author;
-    this.book = book;
+  }
+}
+
+// UI Class: Handle UI Tasks
+class UI {
+  static displayBooks() {
+    const books = Store.getBooks();
+
+    books.forEach((book) => UI.addBookToList(book));
   }
 
-  add() {
-    if (inputAuthor.value !== '' && inputBook.value !== '') {
-      const currentBook = [];
-      currentBook.push(
-        {
-          author: inputAuthor.value,
-          book: inputBook.value,
-        },
-      );
-      this.filter = bookShelf.filter((x) => x.book === currentBook[0].book);
-      if (this.filter.length > 0) {
-        errorMessage.style.display = 'unset';
-        inputAuthor.value = '';
-        inputBook.value = '';
-      }
-      bookShelf.push(
-        {
-          author: inputAuthor.value,
-          book: inputBook.value,
-        },
-      );
-      if (bookShelf.length > 0) {
-        currentBook.forEach((book) => library.insertAdjacentHTML('beforeend', this.displayBooks(book)));
-      }
-    }
-    inputAuthor.value = '';
-    inputBook.value = '';
-    localStorage.setItem('books', JSON.stringify(bookShelf));
+  static addBookToList(book) {
+    const list = document.querySelector('#book-list');
+
+    const bookDisplay = document.createElement('div');
+    bookDisplay.className = 'bookList1';
+    bookDisplay.innerHTML = `
+        <p class="bookTitle"><b>${book.title}</b></p>
+        <p>by<span></span><b>${book.author}.</b></p>
+        <button class="delete">Remove</button>
+        `;
+
+    list.appendChild(bookDisplay);
   }
-  /* eslint-disable */
-  remove() {
-    if (bookShelf.length > 0) {
-      const removebtn = document.querySelectorAll('.remove');
-      removebtn.forEach((element) => element.addEventListener('click', () => {
-        const parentNodeClass = element.parentNode.className;
-        element.parentNode.remove();
-        bookShelf = bookShelf.filter((x) => x.author !== parentNodeClass);
-        localStorage.setItem('books', JSON.stringify(bookShelf));
-      }));
+
+  static deleteBook(el) {
+    if (el.classList.contains('delete')) {
+      el.parentElement.remove();
     }
-  };
+  }
 
-  displayBooks(object){
-    return `<li class="${object.author}">
-      <span class="title">${object.author} by ${object.book}</span>
-      <button class="remove">remove</button></li>`;
-  };
+  static clearFields() {
+    document.querySelector('#title').value = '';
+    document.querySelector('#author').value = '';
+  }
+}
 
-};
+// Store Class: Handles Storage
+class Store {
+  static getBooks() {
+    let books;
+    if (localStorage.getItem('books') === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
+    }
 
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  const newBook = new Book();
-  newBook.add();
-  newBook.remove();
+    return books;
+  }
+
+  static addBook(book) {
+    const books = Store.getBooks();
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+
+  static removeBook(title) {
+    const books = Store.getBooks();
+
+    books.forEach((book, index) => {
+      if (book.title === title) {
+        books.splice(index, 1);
+      }
+    });
+
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+}
+
+// Event: Display Books
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
+
+// Event: Add a Book
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+  // Prevent actual submit
+  e.preventDefault();
+
+  // Get form values
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+
+  // Instatiate book
+  const book = new Book(title, author);
+
+  // Add Book to UI
+  UI.addBookToList(book);
+
+  // Add book to store
+  Store.addBook(book);
+
+  // Clear fields
+  UI.clearFields();
 });
 
-if (storedBooks !== null) {
-  bookShelf = storedBooks;
-  bookShelf.forEach((book) => {
-    const newBook = new Book();
-    library.insertAdjacentHTML('beforeend', newBook.displayBooks(book));
-    newBook.remove();
-  });
-};
+// Event: Remove a Book
+document.querySelector('#book-list').addEventListener('click', (e) => {
+  // Remove book from UI
+  UI.deleteBook(e.target);
+
+  // Remove book from store
+  Store.removeBook(e.target.previousElementSibling.previousElementSibling.textContent);
+});
+
+const currentDate = new Date().toLocaleString();
+document.getElementById('current-date').innerHTML = currentDate;
+
+// display the books list when click the button "List"
+const bookList = document.querySelector('.book-list-container');
+const listBtn = document.querySelector('.listBtn');
+const formContainer = document.querySelector('.form-container');
+
+listBtn.addEventListener('click', () => {
+  bookList.style.display = 'block';
+  formContainer.style.display = 'none';
+});
+
+window.addEventListener('load', () => {
+  bookList.style.display = 'block';
+  formContainer.style.display = 'none';
+  contactInfo.style.display = 'none';
+});
+
+// display the Add book form  when click the button "Add new"
+const addNewBtn = document.querySelector('.add-new-btn');
+
+addNewBtn.addEventListener('click', () => {
+  bookList.style.display = 'none';
+  formContainer.style.display = 'block';
+  contactInfo.style.display = 'none';
+});
+
+// display the  Contact section when click the button "Contact"
+const contactBtn = document.querySelector('.contact');
+const contactInfo = document.querySelector('.contact-info');
+
+contactBtn.addEventListener('click', () => {
+  bookList.style.display = 'none';
+  formContainer.style.display = 'none';
+  contactInfo.style.display = 'block';
+});
